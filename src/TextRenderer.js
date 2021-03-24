a789c0d78db156ecd2eeac6a26ed5b7dded4bc57
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2021-03-24 15:24:34 -0400

    word wrap implemented

--- src/TextRenderer.js
+++ src/TextRenderer.js
@@ -2,25 +2,41 @@ import renderText from './renderFunction';
 import React from 'react';
 import { useRef, useEffect } from 'react';
 
-export default function TextRenderer({ text, bgColor, scale, color, charSpaces, animate }) {
+export default function TextRenderer({ text, bgColor, scale, color, charSpaces, animate, scaleMode, wordWrap }) {
   const canvasRef = useRef(null);
   const defaultText = "sample text"
 
+  function parseWidth(text) {
+    if (!charSpaces) return null;
+    if (!wordWrap) return charSpaces
+    const longestWord = text.split(' ').sort((a, b) => b.length - a.length)[0].length;
+    return (charSpaces < longestWord) ? longestWord : charSpaces;
+  }
+
   useEffect(()=> {
-    renderText(charSpaces * 6 || text?.length * 6 || defaultText.length * 6
+    renderText(parseWidth(text) * 6 || text?.length * 6 || defaultText.length * 6
       , scale || 5
       , canvasRef, text || defaultText
       , color || null
       , animate || false
+      , wordWrap || false
     )
-  })
+  });
+
+  // depending on scale mode, inject different styles into destinationCanvas element
+  const innerStyles = () => {
+    if (!scaleMode || scaleMode === 'fixed') return {background: bgColor} || null ;
+    if (scaleMode === 'auto') return {width: '100%', height: '100%', background: bgColor || null};
+    return null;
+  }
 
   return (
     <div style={{margin: 0, padding: 0, display: 'flex', width: 'auto'}} className="textRendererWrapperInternal">
       <canvas
         ref = {canvasRef}
         className="TextRendererCanvasInternal"
-        style={{background: bgColor || 'none', margin: 0, padding: 0, width: '100%'}}>{text}
+        style={innerStyles()}>
+        {text}
       </canvas>
     </div>
   )
--- src/renderFunction.js
+++ src/renderFunction.js
@@ -1,4 +1,6 @@
-export default function renderText(width, pixelScale, canvasRef, inputText, color, animate=false) {
+import { charDefinitions } from './definitions2';
+
+export default function renderText(width, pixelScale, canvasRef, inputText, color, animate=false, wordWrap = false) {
 
 	// calculate the closest appropriate width based on the input width
 	const remainder = width % 6;
@@ -13,13 +15,19 @@ export default function renderText(width, pixelScale, canvasRef, inputText, colo
 		return;
 	}
 
-	// calculate how many rows needed for the given input text
+	let currentPermittedWidth = 0;
+	let textPixels = [];
+	const characterMaps =  charDefinitions();
+	const  { charWidth } = characterMaps;
 	const horizontalChars = parseInt(width/(6)+1,10);
-	const rows = Math.ceil(inputText.length / horizontalChars);
+	const { starts, words, wastedCharSpaces } = getWordStarts(inputText);
+
+	// calculate how many rows needed for the given input text if we're using charWrap or wordWrap
+	const rows = Math.ceil((inputText.length + (() => wordWrap ? wastedCharSpaces : 0)()) / horizontalChars);
 	// calculate pixel grid height based on input text
 	const height = (rows * 5) + (rows - 1);
 
- 	// set up the text layer canvas
+ 	// set up the canvas
 	const canvas = canvasRef.current
 	if (canvas === null || canvas === undefined) {
 		console.log("couldn't get canvas element")
@@ -29,63 +37,9 @@ export default function renderText(width, pixelScale, canvasRef, inputText, colo
 	ctx.canvas.width = width * pixelScale;
 	ctx.canvas.height = height * pixelScale;
 
-	// character grid coordinates
-	// should move these out to a json file
-	// an editor would be nice for defining more chars...
-	const characterMaps = {
-		'0' : [0, 1, 2, 3, 4, width, width+3, width+4, (width*2), (width*2)+2, (width*2)+4, (width*3), (width*3)+1, (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'1': [1, 2, width+2, (width*2)+2, (width*3)+2, (width*4)+1, (width*4)+2, (width*4)+3],
-		'2': [0, 1, 2, 3, 4, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'3': [0, 1, 2, 3, 4, width+4, (width*2)+2, (width*2)+3, (width*2)+4, (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'4': [0, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3)+4, (width*4)+4],
-		'5': [0, 1, 2, 3, 4, width, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*3)+3, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3],
-		'6': [0, 1, 2, 3, 4, width, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'7': [0, 1, 2, 3, 4, width+4, (width*2)+4, (width*3)+4, (width*4)+4],
-		'8': [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'9': [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3)+4, (width*4)+4],
-		' ': [],
-		'.': [width*4+1],
-		',': [width*4+1, (width*5)+1],
-		"'": [1, width+1],
-		'A' : [2, width+1, width+3, width*2, (width*2)+4, width*3, (width*3)+1, (width*3)+2, (width*3)+3,(width*3)+4, (width*4), (width*4)+4],
-		'B' : [0,1,2,3, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, width*4, (width*4)+1, (width*4)+2, (width*4)+3],
-		'C' : [0, 1, 2, 3, 4, width, (width*2), (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'D' : [0, 1, 2, 3, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4)+1, (width*4)+2, (width*4)+3],
-		'E' : [0, 1, 2, 3, 4, width, (width*2), (width*2), (width*2)+1, (width*2)+2, (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'F' : [0, 1, 2, 3, 4, width, (width*2), (width*2), (width*2)+1, (width*2)+2, (width*3), (width*4), (width*4)],
-		'G' : [0, 1, 2, 3, 4, width, (width*2), (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'H' : [0, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4),(width*4)+4],
-		'I' : [0, 1, 2, 3, 4, width+2, (width*2)+2, (width*3)+2, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'J' : [width+4, width*3, (width*2)+4, (width*3)+4, (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'K' : [0, 4, width, width+3, (width*2), (width*2)+1, (width*2)+2, (width*3), (width*3)+3, (width*4), (width*4),(width*4)+4],
-		'L' : [0, width, (width*2), (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'M' : [0, 4, width, width+1, width+3, width+4, (width*2), (width*2)+2, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4),(width*4)+4],
-		'N' : [0, 4, width, width+1, width+4, (width*2), (width*2)+2, (width*2)+4, (width*3), (width*3)+3, (width*3)+4, (width*4), (width*4),(width*4)+4],
-		'O' : [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'P' : [0,1,2,3, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), width*4],
-		'Q' : [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4, (width*5)+2, (width*3)+2],
-		'R' : [0,1,2,3, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, width*3, (width*3)+3, width*4, (width*4)+4],
-		'S' : [0, 1, 2, 3, 4, width, (width*2), (width*2)+4, (width*2)+1, (width*2)+2, (width*2)+3, (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'T' : [0, 1, 2, 3, 4, width+2, (width*2)+2, (width*3)+2, (width*4)+2],
-		'U' : [0, 4, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-		'V' : [0, 4, width, width+4, (width*2), (width*2)+4, (width*3)+1, (width*3)+3, (width*4)+2],
-		'W' : [0, 4, width, width+4, (width*2), (width*2)+4, width*3, (width*3)+1, (width*3)+3, (width*3)+4, (width*2)+2, width*4, (width*4)+4],
-		'X' : [0, 4, width+1, width+3, (width*2)+2, (width*3)+1, (width*3)+3, (width*2)+2, width*4, (width*4)+4],
-		'Y' : [0, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3)+2, (width*4)+2],
-		'Z' : [0, 1, 2, 3, 4, width+3, (width*2)+2, (width*3)+1, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
-	}
+	wordWrap ? writeWords() : writeString(inputText, textPixels);
+  animate ? animatedDraw() : draw(width, textPixels, ctx, pixelScale);
 
-	let textPixels = [];
-	let remaining = horizontalChars;
-
-	writeString(inputText, textPixels);
-	if (animate) {
-		animatedDraw();
-	} else {
-		draw(width, textPixels, ctx, pixelScale);
-	}
-
-	let currentPermittedWidth = 0;
 	function animatedDraw() {
 		const timerId = setInterval(() => {
 			if (currentPermittedWidth >= width) {
@@ -98,7 +52,60 @@ export default function renderText(width, pixelScale, canvasRef, inputText, colo
 		},20)
 	}
 
+	function getWordStarts(inputText) {
+		let startPosition = 0;
+		let remaining = horizontalChars;
+		let wastedCharSpaces = 0;
+		const words = inputText.toUpperCase().split(' ');
+
+		// create an array with the position of the start of each word
+		const starts = [];
+
+		for (let i = 0; i < words.length; i++) {
+			if (remaining >= words[i].length) {
+				starts.push(startPosition);
+				startPosition += (words[i].length * 6) + 6
+				remaining -= words[i].length + 1;
+			} else {
+				wastedCharSpaces += remaining;
+				// jump to new line
+				startPosition += ((width + 1) * 5) + (remaining - 1) * 6;
+				starts.push(startPosition);
+				startPosition += (words[i].length * 6) + 6;
+				remaining = horizontalChars;
+				remaining -= words[i].length + 1;
+			}
+		}
+		return {
+			starts,
+			words,
+			wastedCharSpaces,
+		}
+	}
+
+	function writeWords() {
+		let charPosition = 0;
+		let channels;
+		starts.forEach((startPosition, i) => {
+			charPosition = 0;
+			Array.from(words[i]).forEach(char => {
+				if (characterMaps.hasOwnProperty(char)) {
+					if (!color) {
+						channels = generateRandomColors()
+					} else {
+						channels = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+					}
+					writeChar(char, textPixels, startPosition + charPosition, channels);
+				} else {
+					writeChar(" ", textPixels, startPosition + charPosition);
+				}
+				charPosition += 6;
+			})
+		});
+	}
+
 	function writeString(inputText, textPixels) {
+		let remaining = horizontalChars;
 		let position = 0;
 		let char;
 		let channels;
@@ -118,36 +125,55 @@ export default function renderText(width, pixelScale, canvasRef, inputText, colo
 			remaining--;
 			if (remaining === 0) {
 				remaining = horizontalChars;
-				position += (width + +1) * 5;
+				position += (width + 1) * 5;
 			} else {
 				position += 6;
 			}
 		}
 	}
 
-function writeChar(char, textPixels, position, color) {
-	characterMaps[char.toUpperCase()].forEach(charPixel => textPixels[charPixel + position] = color);
-}
+	function writeChar(char, textPixels, position, color) {
+		// for defs that include expresions relative to width, like:
+		// { '1': [1, 2, width+2, (width*2)+2, (width*3)+2, (width*4)+1, (width*4)+2, (width*4)+3] }
+		// use the original method:
+		// characterMaps[char.toUpperCase()].forEach(charPixel => textPixels[charPixel + position] = color);
 
-function draw(permittedWidth, textPixels, ctx, pixelScale) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// draw	a frame at a given permitted width
-	const pixelsToDraw = (height * permittedWidth)-1;
+		// for absolute coordinates like:
+		// { '1': [1, 2, 7, 12, 17, 21, 22, 23] }
+		// we'll need to infer when to add the width of the canvas to them
+		// take a coordinate
+		// divide by the charater width (5 in this case)
+		// round the result down
+		// Math.floor(coord / charWidth) === row
+		// coord % charWidth === the position inside the row
+		// this will allow us to store char defs as numbers, not as numbers mixed with expressions
+		// should be easier to store and generate programatically
+		// but if the character width changes, we'll need new defs and need to pass in the charWidth to this function
 
-	// let counter = 0;
-	outer: for (let column = 0; column < height; column++) {
-						for (let row = 0; row < permittedWidth; row++){
-							// which pixel do I select from textPixels based on column and row?
-							let pixelIndex = row + (column*permittedWidth)
-							if (pixelIndex > pixelsToDraw) break outer;
-							if (textPixels[pixelIndex] !== undefined) {
-								ctx.fillStyle = textPixels[pixelIndex];
-								ctx.fillRect(row*pixelScale, column*pixelScale, pixelScale, pixelScale);
-								// counter++;
-							}
-						}
+		characterMaps[char.toUpperCase()].forEach(charPixel => {
+			const charRow = Math.floor(charPixel / charWidth)
+			const offset = charPixel % charWidth;
+			textPixels[(charRow * width) + offset + position] = color;
+		});
 	}
-}
+
+	function draw(permittedWidth, textPixels, ctx, pixelScale) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		// draw	a frame at a given permitted width
+		const pixelsToDraw = (height * permittedWidth)-1;
+
+		outer: for (let column = 0; column < height; column++) {
+							for (let row = 0; row < permittedWidth; row++){
+								// which pixel do I select from textPixels based on column and row?
+								let pixelIndex = row + (column*permittedWidth)
+								if (pixelIndex > pixelsToDraw) break outer;
+								if (textPixels[pixelIndex] !== undefined) {
+									ctx.fillStyle = textPixels[pixelIndex];
+									ctx.fillRect(row*pixelScale, column*pixelScale, pixelScale, pixelScale);
+								}
+							}
+		}
+	}
 
 	function generateRandomColors(){
     const random = () => {
@@ -160,4 +186,5 @@ function draw(permittedWidth, textPixels, ctx, pixelScale) {
       const color = `rgb(${R}, ${G}, ${B})`;
       return color;
   }
+
 }
