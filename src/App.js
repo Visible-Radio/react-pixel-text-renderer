3e2400f3d9db09409dafa6ef9eeb7e8634f95b0b
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2022-04-18 15:23:07 -0400

    add chars to charset

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import { syncTextRenderer } from "./new/renderSingleChar";
 const text =
   "Nisi dolore eiusmod veniam consectetur. Officia ut sit irure adipisicing adipisicing ad velit ad magna adipisicing aliquip. Non aute minim minim excepteur tempor excepteur aliqua. Pariatur cupidatat amet esse aliquip veniam eu duis amet commodo. Quis aute exercitation minim reprehenderit in qui quis adipisicing ad aliquip. Dolore velit quis fugiat elit aliqua.";
 
+const joke = "Welcome to Node pixel text renderer v0.0.1. init charset. ...";
+const end = "Charset initialized. ... Hello world";
+
 function App() {
   useEffect(() => {
     syncTextRenderer({
       columns: 24,
       displayRows: 5,
-      scale: 2,
-      text: Object.keys(defs).join(" ").toUpperCase(),
+      scale: 3,
+      text: (joke + Object.keys(defs).join(" ") + end).toUpperCase(),
       defs,
     });
   }, []);
