f99c9d52c802d84c8b1286c7e5a4e0f602790da6
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2022-04-18 13:48:00 -0400

    add chars to charset

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import { useEffect } from "react";
-import { defs } from "./customDefs_cw_7";
+// import { defs } from "./customDefs_cw_7";
+import defs from "./new/customDefs_charWidth_7.json";
 import { syncTextRenderer } from "./new/renderSingleChar";
+
+const text =
+  "Nisi dolore eiusmod veniam consectetur. Officia ut sit irure adipisicing adipisicing ad velit ad magna adipisicing aliquip. Non aute minim minim excepteur tempor excepteur aliqua. Pariatur cupidatat amet esse aliquip veniam eu duis amet commodo. Quis aute exercitation minim reprehenderit in qui quis adipisicing ad aliquip. Dolore velit quis fugiat elit aliqua.";
 
 function App() {
   useEffect(() => {
     syncTextRenderer({
-      columns: 9,
+      columns: 24,
       displayRows: 5,
-      scale: 5,
-      text: "hello world whats cookin hello world whats cookin".toUpperCase(),
+      scale: 2,
+      text: Object.keys(defs).join(" ").toUpperCase(),
       defs,
     });
   }, []);
